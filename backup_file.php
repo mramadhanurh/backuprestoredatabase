342f630fd3bffe93edf5b672821d2f18ea919a25
mramadhanurh <mramadhanur@gmail.com>
2022-07-13 10:31:28 +0700

    upload project

---
+++ backup_file.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backup Database</title>
+</head>
+<body>
+
+    <a href="db.php" type="button">Download Database</a>
+    <input type='button'value='Download Backup'onClick='top.location="db.php"'>
+
+</body>
+</html>
